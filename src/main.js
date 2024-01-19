538179e64802a6f99076fd248487d3e1fb513eca
sahiddjinadou <djinadousahid@gmail.com>
2024-01-19 20:41:00 +0100

    initialisation  first commit

---
+++ src/main.js
@@ -0,0 +1,25 @@
+// import './assets/main.css'
+
+// import { createApp } from 'vue'
+// import App from './App.vue'
+// import router from './router'
+
+// const app = createApp(App)
+
+// app.use(router)
+
+// app.mount('#app')
+
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import store from "./store";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from './plugins/font-awesome'
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .mount("#app");
